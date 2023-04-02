6c2654dfc8d29994b0c77feb9d9add89e394bfab
Kim Wong <15217035@life.hkbu.edu.hk>
2023-04-02 14:52:58 -0400

    update newline replacement

--- util.js
+++ util.js
@@ -8,7 +8,7 @@ const parseJSON = (jsonString) => {
     if (error instanceof SyntaxError) {
       logger.warn("utils parseJSON met a SyntaxError:", error);
 
-      let updatedJsonString = jsonString.replace(/\\n|[\n]/g, "\\n");
+      let updatedJsonString = jsonString.replace(/\\n|[\n]/g, "");
 
       logger.warn("utils parseJSON updatedJsonString:", updatedJsonString);
 
